a684c34211577f0703ef59ae448b872156039662
Ahmad Raza <ahmadraza330022@gmail.com>
2026-08-05 22:07:34 +0500

    feat: integrate WonderBrainFactory into orchestration pipeline

--- src/services/AssetEngine.ts
+++ src/services/AssetEngine.ts
@@ -1,36 +1,25 @@
 import type { StoryRecord } from "../types/story"
 import type { LearningPackage } from "./ai/learningPackage"
+import type { StoryBook } from "../types/storybook"
+import type { StoryNarration } from "../types/narration"
 
 export interface StoryAssets {
-
-  storyBook: unknown
-
-  narration: unknown
-
+  storyBook: StoryBook
+  narration: StoryNarration
   illustrations: unknown
-
 }
 
 export class AssetEngine {
-
   async build(
     _story: StoryRecord,
     _learning: LearningPackage
   ): Promise<StoryAssets> {
-
     return {
-
-      storyBook: null,
-
-      narration: null,
-
+      storyBook: { title: "Untitled", pages: [] },
+      narration: { title: "Untitled", language: "en", segments: [] },
       illustrations: null,
-
     }
-
   }
-
 }
 
-export const assetEngine =
-  new AssetEngine()
+export const assetEngine = new AssetEngine()
